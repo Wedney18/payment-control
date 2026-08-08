52293ac9f48e63b43c26dc26ffc1fb96045ff086
Wedney <wedney.santos@gmail.com>
2026-08-08 17:20:49 -0300

    Adicionando arquivos login.json, login.js e refatoração no arquivo login.test.js

--- payment-control/test/specs/mutations/login.test.js
+++ payment-control/test/specs/mutations/login.test.js
@@ -1,21 +1,12 @@
 const request = require('supertest')
 const { expect } = require('chai')
+const { login } = require('../../helpers/login.js')
+const loginData = require('../../fixtures/login.json')
 
 describe('Mutation - Login', () => {
-    it('deve realizar login com sucesso quando informo credenciais válidas', async () => {
-        const resposta = await request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-                query: `mutation Login($email: String!, $senha: String!) {
-                    login(email: $email, senha: $senha) {
-                        token
-                    }
-                }`,
-                variables: {  
-                    email: "admin@admin.com",
-                    senha: "123456"
-                }
-            })
+
+    it('deve realizar login com sucesso quando informo credenciais válidas', async () => {                
+        const resposta = await login(loginData.admin)
         expect(resposta.status).to.equal(200)
         expect(resposta.body.data.login).to.have.property('token')
         expect(resposta.body.data.login.token).to.not.be.empty
@@ -24,71 +15,28 @@ describe('Mutation - Login', () => {
     })
 
     it('não deve realizar login quando informo credenciais inválidas', async () => {
-        const resposta = await request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-                query: `mutation Login($email: String!, $senha: String!) {
-                    login(email: $email, senha: $senha) {
-                        token
-                    }
-                }`,
-                variables: {  
-                    email: "admin@admin.com",
-                    senha: "1234567"
-                }
-            })
+        const usuario = { ...loginData.admin, senha: '1234567' }
+        const resposta = await login(usuario)
         expect(resposta.status).to.equal(200)
         expect(resposta.body.errors[0]).to.have.property('message', 'Credenciais inválidas ou usuário inativo.')
     })
 
     it('não deve realizar login quando não informo o email', async () => {
-        const resposta = await request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-                query: `mutation Login($email: String!, $senha: String!) {
-                    login(email: $email, senha: $senha) {
-                        token
-                    }
-                }`,
-                variables: {  
-                    senha: "123456"
-                }
-            })
+        const { email, ...usuario } = loginData.admin
+        const resposta = await login(usuario)
         expect(resposta.status).to.equal(400)
         expect(resposta.body.errors[0]).to.have.property('message', 'Variable \"$email\" of required type \"String!\" was not provided.')
     })
 
     it('não deve realizar login quando não informo a senha', async () => {
-        const resposta = await request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-                query: `mutation Login($email: String!, $senha: String!) {
-                    login(email: $email, senha: $senha) {
-                        token
-                    }
-                }`,
-                variables: {  
-                    email: "admin@admin.com"
-                }
-            })
+        const { senha, ...usuario } = loginData.admin        
+        const resposta = await login(usuario)
         expect(resposta.status).to.equal(400)
         expect(resposta.body.errors[0]).to.have.property('message', 'Variable \"$senha\" of required type \"String!\" was not provided.')
     })
 
     it('não deve realizar login quando informo credenciais de um usuário inativo', async () => {
-        const resposta = await request('http://localhost:4000')
-            .post('/graphql')
-            .send({
-                query: `mutation Login($email: String!, $senha: String!) {
-                    login(email: $email, senha: $senha) {
-                        token
-                    }
-                }`,
-                variables: {  
-                    email: "pgats@turma3.com",
-                    senha: "123456"
-                }
-            })
+        const resposta = await login(loginData.pgats)
         expect(resposta.status).to.equal(200)
         expect(resposta.body.errors[0]).to.have.property('message', 'Credenciais inválidas ou usuário inativo.')
     })
